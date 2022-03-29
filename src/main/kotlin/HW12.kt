0271f68a5379da69bf82833b92387d136abc5d65
serg-nao <serg-nao@rambler.ru>
2022-03-29 22:02:31 +0300

    Code changed

--- src/main/kotlin/HW12.kt
+++ src/main/kotlin/HW12.kt
@@ -1,14 +1,11 @@
 package ru.netology
 
 fun main() {
-    val amount1 = 1_000_00
-    val amount2 = 10_000_00
+    val amount = 1_000_00
     val tax = 0.0075
     val minTax = 35_00
 
-    val totalTax1 = if (amount1 * tax > minTax) amount1 * tax else minTax
-    val totalTax2 = if (amount2 * tax > minTax) amount1 * tax else minTax
+    val totalTax = if (amount * tax > minTax) amount * tax else minTax
 
-    println("Tax of amount $amount1 is: $totalTax1")
-    println("Tax of amount $amount2 is: $totalTax2")
+    println("Tax of amount $amount is: $totalTax")
 }
